f3bb4a650064f0e8ea9bb0c648d4bcda6719e0bc
Dieggo Carrilho <dieggopsc@gmail.com>
2018-09-13 01:18:40 -0300

    Criação de estrutura e arquitetura para o sistema

---
+++ src/main/java/com/br/dieggocarrilho/linhas/LinhasApplication.java
@@ -0,0 +1,12 @@
+package com.br.dieggocarrilho.linhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinhasApplication.class, args);
+	}
+}
