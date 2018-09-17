69532de3f4aa7bbb4dfead2fabf5f31db6d0d8f1
Dieggo Siqueira <dieggo.p.de.sique@accenture.com>
2018-09-17 09:53:32 -0300

    Solução para recuperar Intinerarios

--- src/main/java/com/br/dieggocarrilho/linhas/domain/Intinerario.java
+++ src/main/java/com/br/dieggocarrilho/linhas/domain/Intinerario.java
@@ -3,37 +3,31 @@ package com.br.dieggocarrilho.linhas.domain;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import java.io.Serializable;
-import java.util.List;
 
-@Entity
+import com.google.gson.internal.LinkedHashTreeMap;
+
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Intinerario implements Serializable {
 
     private static final long serialVersionUID = -4181985100899233094L;
 
-    @Id
-    private Long id;
     Long idlinha;
 
     String codigo;
     String nome;
-    Double latitude;
-    Double longitude;
 
-    @OneToMany
-    List<Coordenadas> coordenadas;
+    Map<String, Object> coordenadas = new HashMap<String, Object>();
+
 
     public Intinerario() {
     }
 
-    public Intinerario(Long idlinha, String codigo, String nome, Double latitude, Double longitude, List<Coordenadas> coordenadas) {
-        this.idlinha = idlinha;
-        this.codigo = codigo;
-        this.nome = nome;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.coordenadas = coordenadas;
-    }
+     
 
     public Long getIdlinha() {
         return idlinha;
@@ -59,56 +53,12 @@ public class Intinerario implements Serializable {
         this.nome = nome;
     }
 
-    public List<Coordenadas> getCoordenadas() {
-        return coordenadas;
-    }
 
-    public void setCoordenadas(List<Coordenadas> coordenadas) {
-        this.coordenadas = coordenadas;
-    }
 
-    public Long getIdLinha() {
-        return idlinha;
-    }
-
-    public void setIdLinha(Long idLinha) {
-        this.idlinha = idLinha;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Override
-    public String toString() {
-        return "Intinerario{" +
-                "id=" + id +
-                ", idlinha=" + idlinha +
-                ", codigo='" + codigo + '\'' +
-                ", nome='" + nome + '\'' +
-                ", latitude=" + latitude +
-                ", longitude=" + longitude +
-                ", coordenadas=" + coordenadas +
-                '}';
-    }
+	@Override
+	public String toString() {
+		return "Intinerario [idlinha=" + idlinha + ", codigo=" + codigo + ", nome=" + nome + ", coordenadas="
+				+ coordenadas + "]";
+	}
+ 
 }
